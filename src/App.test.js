2c30996bfc9619b4436dd2b1ecadf462702d8a18
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-07-29 00:55:55 -0500

    Setup Enzyme and Write Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug());
 });
+
+test('renders increment button', () => {});
+
+test('render counter element', () => {});
+
+test('counter starts at 0 ', () => {});
+
+test('counter increments on user action', () => {});
