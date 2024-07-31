b09888a4032e210bc73a961a1c3676b0e8496a44
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-07-31 01:45:03 -0500

    DRY Refactor

--- src/App.test.js
+++ src/App.test.js
@@ -7,21 +7,34 @@ import App from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find('[data-test="app-component"]');
+/**
+ * Factory function to create a shallowWrapper for the App Component
+ * @@function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />);
+};
+
+const findByTestAttribute = (wrapper, value) => {
+  return wrapper.find(`[data-test="${value}"]`);
+};
+
+test('renders app component without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttribute(wrapper, 'app-component');
   expect(appComponent.length).toBe(1);
 });
 
 test('render counter heading', () => {
-  const wrapper = shallow(<App />);
-  const counterHeading = wrapper.find('[data-test="counter-heading"]');
+  const wrapper = setup();
+  const counterHeading = findByTestAttribute(wrapper, 'counter-heading');
   expect(counterHeading.length).toBe(1);
 });
 
 test('renders increment button', () => {
-  const wrapper = shallow(<App />);
-  const incrementButton = wrapper.find('[data-test="increment-button"]');
+  const wrapper = setup();
+  const incrementButton = findByTestAttribute(wrapper, 'increment-button');
   expect(incrementButton.length).toBe(1);
 });
 
