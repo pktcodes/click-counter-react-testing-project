5c67c1493f0813ccf7f38f1831cfee47492c79e3
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-11-13 23:43:25 -0600

    Add Styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 
+import './App.css';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App" data-test="app-component">
-      <h1 data-test="counter-heading">
-        The counter currently is
-        <span data-test="count-value">{count}</span>
+      <h1 data-test="counter-heading" className="counter-heading">
+        The counter currently is&nbsp;
+        <span data-test="count-value" className="count-value">
+          {count}
+        </span>
       </h1>
-      <button data-test="increment-button" onClick={() => setCount(count + 1)}>
+      <button
+        data-test="increment-button"
+        className="increment-button"
+        onClick={() => setCount(count + 1)}
+      >
         Increment counter
       </button>
     </div>
